5b21c5866e962857d6bdbbd8f420b485d15df86c
Andrew Roth <andrewjlroth@gmail.com>
2026-04-17 16:53:04 -0700

    envs: pinning versions

--- envs/signals.post-deploy.sh
+++ envs/signals.post-deploy.sh
@@ -1,4 +1,4 @@
 #!env bash
 set -o pipefail
 
-Rscript -e "library(devtools); pak::pak('shahcompbio/signals', upgrade=FALSE, dependencies=FALSE)"
+Rscript -e "library(devtools); pak::pak('shahcompbio/signals@v0.14.1', upgrade=FALSE, dependencies=FALSE)"
